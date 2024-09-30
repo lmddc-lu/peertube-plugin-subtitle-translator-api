12fa9188d19e042b617d649299a035b3b08c128c
Nathan Lenas <nathan.lenas@lmddc.lu>
2024-09-30 14:29:07 +0200

    Translation endpoint

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-PHONY: build run
+PHONY: build run stop
+
+stop:
+	-docker stop subtitle_translator_api
 
 build:
 	docker build -t subtitle_translator_api .
 
-run: build
+run: stop build
 	docker run -p 5000:80 subtitle_translator_api
--- app/main.py
+++ app/main.py
@@ -1,15 +1,34 @@
 from typing import Union
+from fastapi import FastAPI, File, UploadFile
 
-from fastapi import FastAPI
+from subtitles_translator.available_languages import AvailableLanguages
+from subtitles_translator.subtitles import Subtitles
+from subtitles_translator.translator import Translator
+
+import uuid
+import os
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/available_languages/{language_code}")
+def get_available_languages(language_code: str):
+    print(str(AvailableLanguages(language_code)))
+    
+    return {"language": str(AvailableLanguages(language_code))}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/translate_srt/{language_from}/{language_to}")
+async def translate_srt(language_from: str, language_to: str, file: UploadFile):
+    original_language = AvailableLanguages(language_from)
+    target_language = AvailableLanguages(language_to)
+    translator = Translator(original_language, target_language)
+    content = await file.read()
+    content = content.decode('utf-8')  
+    subtitles = Subtitles(content.splitlines())
+    translator.translate_subtitles(subtitles)
+    file_name = f"{uuid.uuid4()}.srt"
+    subtitles.save_srt(file_name)
+    with open(file_name, "rb") as file:
+        translated_srt = file.read()
+    os.remove(file_name)
+    return {"translated_srt": translated_srt}
