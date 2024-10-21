86f4cf61e788c16fc859cb6206594276ac8b0081
Nathan Lenas <nathan.lenas@lmddc.lu>
2024-10-21 13:21:03 +0200

    Add .env to build appropriate image, fix caching for cpu image

--- Makefile
+++ Makefile
@@ -1,16 +1,32 @@
-PHONY: build run stop
+.PHONY: build-gpu build-cpu stop run-cpu run-gpu run
+
+include .env
 
 stop:
-	-docker stop subtitle_translator_api
+	-docker stop subtitle_translator_api_cpu 2> /dev/null 
+	-docker stop subtitle_translator_api_gpu 2> /dev/null
+	-docker rm subtitle_translator_api_cpu 2> /dev/null
+	-docker rm subtitle_translator_api_gpu 2> /dev/null
 
-build:
+# This requires poetry and python 3.9 installed
+build-subtitle-translator:
+	cd vendor/Subtitles-Translator && poetry install && poetry build
+
+build-cpu:
 	docker build -t subtitle_translator_api -f ./Dockerfile.cpu .
 
 build-gpu:
 	docker build -t subtitle_translator_api -f ./Dockerfile.gpu .
 
-run: stop build
-	docker run -d -p 5000:80 -v data:/code/data subtitle_translator_api
+run-cpu: stop build-cpu
+	docker run --name subtitle_translator_api_cpu -d -p $(API_PORT):80 -v data:/code/data subtitle_translator_api
 
 run-gpu: stop build-gpu
-	docker run --gpus all -d -p 5000:80 -v data:/code/data subtitle_translator_api
+	docker run --name subtitle_translator_api_gpu --gpus all -d -p $(API_PORT):80 -v data:/code/data subtitle_translator_api
+
+run:
+	if [ $(USE_GPU) = "true" ]; then \
+		$(MAKE) run-gpu; \
+	else \
+		$(MAKE) run-cpu; \
+	fi
--- app/main.py
+++ app/main.py
@@ -15,8 +15,9 @@ import logging
 import json
 
 # Use GPU
-# import torch
-# torch.set_default_tensor_type('torch.cuda.FloatTensor')
+if os.environ.get('USE_GPU', 'false') == 'true':
+  import torch
+  torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -26,13 +27,7 @@ app = FastAPI()
 
 #From: https://github.com/UKPLab/EasyNMT?tab=readme-ov-file#opus-mt
 languagePool = ['aav', 'aed', 'af', 'alv', 'am', 'ar', 'art', 'ase', 'az', 'bat', 'bcl', 'be', 'bem', 'ber', 'bg', 'bi', 'bn', 'bnt', 'bzs', 'ca', 'cau', 'ccs', 'ceb', 'cel', 'chk', 'cpf', 'crs', 'cs', 'csg', 'csn', 'cus', 'cy', 'da', 'de', 'dra', 'ee', 'efi', 'el', 'en', 'eo', 'es', 'et', 'eu', 'euq', 'fi', 'fj', 'fr', 'fse', 'ga', 'gaa', 'gil', 'gl', 'grk', 'guw', 'gv', 'ha', 'he', 'hi', 'hil', 'ho', 'hr', 'ht', 'hu', 'hy', 'id', 'ig', 'ilo', 'is', 'iso', 'it', 'ja', 'jap', 'ka', 'kab', 'kg', 'kj', 'kl', 'ko', 'kqn', 'kwn', 'kwy', 'lg', 'ln', 'loz', 'lt', 'lu', 'lua', 'lue', 'lun', 'luo', 'lus', 'lv', 'map', 'mfe', 'mfs', 'mg', 'mh', 'mk', 'mkh', 'ml', 'mos', 'mr', 'ms', 'mt', 'mul', 'ng', 'nic', 'niu', 'nl', 'no', 'nso', 'ny', 'nyk', 'om', 'pa', 'pag', 'pap', 'phi', 'pis', 'pl', 'pon', 'poz', 'pqe', 'pqw', 'prl', 'pt', 'rn', 'rnd', 'ro', 'roa', 'ru', 'run', 'rw', 'sal', 'sg', 'sh', 'sit', 'sk', 'sl', 'sm', 'sn', 'sq', 'srn', 'ss', 'ssp', 'st', 'sv', 'sw', 'swc', 'taw', 'tdt', 'th', 'ti', 'tiv', 'tl', 'tll', 'tn', 'to', 'toi', 'tpi', 'tr', 'trk', 'ts', 'tum', 'tut', 'tvl', 'tw', 'ty', 'tzo', 'uk', 'umb', 'ur', 've', 'vi', 'vsl', 'wa', 'wal', 'war', 'wls', 'xh', 'yap', 'yo', 'yua', 'zai', 'zh', 'zne']
-# languagePool = ["fr","en", "es", "de", "ru", "ar", "hi", "it", "zh", "nl", "pt"]
-  
 
-# @app.get("/existing_language_pairs/cached")
-# def get_language_pairs():
-#     global lp
-#     return lp
 
 def check_language_pair_exists(lang1: str, lang2: str) -> bool:
     """
